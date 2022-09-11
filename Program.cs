af727e8e0f0c365c97d64f4e3f8bcfcdf4e4360e
Roman <dorofeevrs1983@gmail.com>
2022-09-11 20:37:37 +0700

    Задаём произвольный массив строк

--- Program.cs
+++ Program.cs
@@ -2,36 +2,4 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись 
 // исключительно массивами
 
-
-string[] sourcearray = {"Good day", "5", "World", "a", "Free"};
-
-Console.WriteLine('[' + string.Join(",", sourcearray) + ']');
-
-string s = "";
-
-int n = 0;
-
-for (int i = 0; i < sourcearray.GetLength(0); i++)
-{
-    s = sourcearray[i];
-    if (s.Length < 4 & s.Length > 0)
-    n++;
-}
-Console.WriteLine(n);
-
-
-
-string[] newarr = new string[n];
-
-for (int i = 0; i < n; i++)
-{
-
-    s = sourcearray[i];
-    if (s.Length < 4 & s.Length != 0)
-    {
-    newarr [i] = sourcearray[i];
-    }
-    
-}
-
-Console.WriteLine('[' + string.Join(",", newarr) + ']');
+string[] sourcearray = {"Good day", "1", "World", "You", "Free", "18", "Me"};
